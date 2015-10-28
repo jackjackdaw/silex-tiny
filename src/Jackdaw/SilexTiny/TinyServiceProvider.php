f4b02f85520ce2d4393a1e4036890825b082d3d4
Jack Jackdaw <jackdaw@ihme.org>
2015-10-28 10:13:18 +0200

    Moved the options to a different identifier.

--- src/Jackdaw/SilexTiny/TinyServiceProvider.php
+++ src/Jackdaw/SilexTiny/TinyServiceProvider.php
@@ -24,21 +24,21 @@ class TinyServiceProvider implements ServiceProviderInterface
      */
     public function register(Container $app)
     {
+        // Register the tiny service
         $app['tiny'] = function ($app) {
             // Make sure a tiny set has been setup.
-            if (! isset($app['tiny.set'])) {
+            if (! isset($app['tiny.options']['set'])) {
                 throw new \RuntimeException('You must provide a random alpha-numeric set where each character must only be used exactly once. See https://github.com/zackkitzmiller/tiny-php for more info.');
             }
 
-            return new Tiny($app['tiny.set']);
+            return new Tiny($app['tiny.options']['set']);
         };
 
-        // Add the twig markdown extension
+        // Add the twig tiny extension.
         $app['twig'] = $app->extend('twig', function ($twig, $app) {
             // Register the tiny Twig extension.
             $twig->addExtension(new TinyExtension($app['tiny']));
             return $twig;
         });
-
     }
 }
