5c59894d99eb497a07e399feb551d2ea46a1d39a
Jack Jackdaw <jackdaw@ihme.org>
2015-10-28 09:59:02 +0200

    Added a Twig filter extension for tiny.

---
+++ src/Jackdaw/SilexTiny/Twig/TinyExtension.php
@@ -0,0 +1,56 @@
+<?php
+namespace Jackdaw\SilexTiny\Twig;
+
+use League\Tiny\Tiny;
+
+/**
+ * A Tiny extension for Twig.
+ * @author jackjackdaw
+ *
+ */
+class TinyExtension extends \Twig_Extension
+{
+    /**
+     * The tiny class.
+     * @var League\Tiny\Tiny
+     */
+    protected $tiny;
+
+    /**
+     *
+     * @param Tiny $tiny
+     */
+    public function __construct(Tiny $tiny)
+    {
+        $this->tiny = $tiny;
+    }
+
+    /**
+     * Add the tiny filter.
+     * @return multitype:\Twig_SimpleFilter
+     */
+    public function getFilters()
+    {
+        return array(
+            new \Twig_SimpleFilter('tiny', array($this, 'tiny')),
+        );
+    }
+
+    /**
+     * Do the tiny conversion.
+     * @param string $text
+     */
+    public function tiny($id)
+    {
+        return $this->tiny->to($id);
+    }
+
+    /**
+     * Get the extension name.
+     * @return string
+     */
+    public function getName()
+    {
+        return 'tiny';
+    }
+}
